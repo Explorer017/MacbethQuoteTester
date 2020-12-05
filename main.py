5811495d3212279203cffd9cb123e86c9ebf2d71
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2020-12-05 13:55:27 +0000

    Added comma and exclamation point remover, added support for new .quotes file

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ import random as r
 import csv
 
 with open("assets/macbeth.quotes", encoding="utf8") as f:
-		read = csv.reader(f)
-		quotes = list(read)
-
+    read = csv.reader(f, delimiter='|')
+    quotes = list(read)
 
 
 class MacbethQuotes:
+    points = 0
 
     def easy(self):
         print("Coming soon!")
 
     def normal(self):
         lives = 3
-        name = input("What is your name?")
+        name = input("What is your name? ")
+        print("\n")
         while True:
 
-            global points
-
             q = r.randint(0, len(quotes) - 1)
-            phrase = quotes[q][0].split()
+            phrase = quotes[q][1].split()
             for i in range(len(phrase)):
                 newWord = ""
                 for k in range(len(phrase[i])):
@@ -34,28 +33,42 @@ class MacbethQuotes:
                     newWord = newWord + LetterToPut
                 phrase[i] = newWord
 
-
             remword = r.randint(0, len(phrase) - 1)
-            dash = ""
-            remwordstr = phrase[remword]
+            newWord = ""
+            for l in range(len(phrase[remword])):
+                if phrase[remword][l] == ",":
+                    LetterToPut = ""
+                elif phrase[remword][l] == "!":
+                    LetterToPut = ""
+                else:
+                    LetterToPut = phrase[remword][l]
+                newWord = newWord + LetterToPut
+            remwordstr = newWord
 
+            dash = ""
+            newWord = ""
             for j in range(len(phrase[remword])):
-                dash = dash + "_"
+                if phrase[remword][j] == ",":
+                    dash = dash + ","
+                elif phrase[remword][j] == "!":
+                    dash = dash + "!"
+                else:
+                  dash = dash + "_"
 
             phrase[remword] = dash
             phrase = " ".join(phrase)
             print(phrase)
             ans = input("What is the missing word from the quote?\n")
             if ans.lower() == remwordstr.lower():
-                points = points + 1
-                print(f"\nWell done, {name}! You have {points} point(s), and {lives} lives left.\n")
+                self.points = self.points + 1
+                print(f"\nWell done, {name}! You have {self.points} point(s), and {lives} lives left.\n")
             else:
                 lives = lives - 1
                 if lives < 0:
-                    print(f"\nSorry, {name}. Your're out of lives. You got {points} point(s)")
+                    print(f"\nSorry, {name}. Your're out of lives. You got {self.points} point(s)")
                     print(f"\nThe correct answer is {remwordstr}")
                     break
-                print(f"\nIncorrect answer, {name}. Better luck next time! You have {points} point(s), "
+                print(f"\nIncorrect answer, {name}. Better luck next time! You have {self.points} point(s), "
                       f"and {lives} lives left.\n")
                 print(f"The correct answer is {remwordstr}\n")
 
@@ -74,20 +87,16 @@ class MacbethQuotes:
         print("Quotes from William Shakespeare's 'Macbeth'")
         s(1)
         print("Current Version - 0.0.1")
+        print("Follow me on GitHub! https://github.com/Explorer017")
 
-    def __init__(self):
-        global points
+    def menu(self):
         while True:
-            points = 0
-            print(open("assets/macbeth.logo", "r", encoding="UTF-8").read())
-            print("\nQuote Tester - [0.0.1]")
-            s(1)
-            print("By Finn O'Neill, 2020\n")
-            s(1)
+            self.points = 0
             picked = False
             while not picked:
                 try:
-                    print("What would you like to do?\n1) Easy Mode\n2) Normal Mode\n3) Hard Mode\n4) Look at the leaderboard [BETA]\n5) About")
+                    print(
+                        "What would you like to do?\n1) Easy Mode\n2) Normal Mode\n3) Hard Mode\n4) Look at the leaderboard [BETA]\n5) About")
                     option = int(input())
                     picked = True
                 except:
@@ -111,17 +120,16 @@ class MacbethQuotes:
                     picked = True
                 else:
                     print("An error occurred! Make sure your putting in numbers within the given range!")
-            print("Thanks for playing! Would you like to try again?")
-            answer = input().lower()
 
-            picked = False
-            while not picked:
-                if answer == "y":
-                    picked = True
-                elif answer == "n":
-                    exit(print("Bye Bye!"))
-                else:
-                    print("Sorry, I don't understand this!")
+    def __init__(self):
+        print(open("assets/macbeth.logo", "r", encoding="UTF-8").read())
+        print("\nQuote Tester - [0.0.1]")
+        s(1)
+        print("By Finn O'Neill, 2020\n")
+        s(1)
+        self.menu()
+
+
 
 
 
